c7fffdeb35372e2bd4ef9ff88477c87fb5db5909
rajathraofed <rrao@sitetracker.com>
2021-03-29 12:40:40 -0700

    adding all files

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      randomcolor: null
+    }
+  }
+  getRandomColor = () => {
+    return (
+      "rgb(" +
+      Math.floor((Math.random() * 256)) + "," +
+      Math.floor((Math.random() * 256)) + "," +
+      Math.floor((Math.random() * 256)) + ")"
+    );
+  }
+  myButtonPressed = () => {
+    this.setState({ randomcolor: this.getRandomColor() })
+  }
+
+  render() {
+    return (
+
+      <View style={[styles.container, { backgroundColor: this.state.randomcolor }]}>
+        <TouchableOpacity onPress={this.myButtonPressed}>
+          <Text style={[styles.text, { backgroundColor: this.getRandomColor() }]}>Login </Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +43,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 50,
+    backgroundColor: "#BB2CD9",
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    color: "#FFFFFF",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+
+
+  }
 });
